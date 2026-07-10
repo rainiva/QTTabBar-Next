dd0dcb86e6779acfff4bffa61e3d816898ce510c
rainiva <coxtea@foxmail.com>
2026-07-10 21:02:21 +0800

    refactor(shutdown): remove root controller dependency

--- QTTabBar/QTTabBarClass.ComponentBuildController.cs
+++ QTTabBar/QTTabBarClass.ComponentBuildController.cs
@@ -65,7 +65,7 @@ namespace QTTabBarLib {
                 _owner._folderTreeController = new FolderTreeController((IFolderTreeHost)_owner);
                 _owner._viewModeController = new ViewModeController((IViewModeHost)_owner);
                 _owner._pluginMenuController = new PluginMenuController((IPluginMenuHost)_owner);
-                _owner._shutdownController = new ShutdownController(_owner);
+                _owner._shutdownController = new ShutdownController((IShutdownResourceHost)_owner, (IShutdownPersistenceHost)_owner);
                 _owner.tabControl1.RowCountChanged += _owner.tabControl1_RowCountChanged;
                 _owner.tabControl1.Deselecting += _owner.tabControl1_Deselecting;
                 _owner.tabControl1.Selecting += _owner.tabControl1_Selecting;
--- QTTabBar/QTTabBarClass.ShutdownAccess.cs
+++ QTTabBar/QTTabBarClass.ShutdownAccess.cs
@@ -6,7 +6,31 @@ using System.Windows.Forms;
 using QTTabBarLib.Interop;
 
 namespace QTTabBarLib {
-    public partial class QTTabBarClass {
+    public partial class QTTabBarClass : IShutdownResourceHost, IShutdownPersistenceHost {
+        QTabControl IShutdownResourceHost.TabControl => tabControl1;
+        TreeViewWrapper IShutdownResourceHost.TreeViewWrapper { get => treeViewWrapper; set => treeViewWrapper = value; }
+        ListViewMonitor IShutdownResourceHost.ListViewManager { get => listViewManager; set => listViewManager = value; }
+        SubDirTipForm IShutdownResourceHost.SubDirTip { get => subDirTip_Tab; set => subDirTip_Tab = value; }
+        QTTabBarClass.PluginServer IShutdownResourceHost.PluginServer { get => pluginServer; set => pluginServer = value; }
+        NativeWindowController IShutdownResourceHost.ExplorerController { get => explorerController; set => explorerController = value; }
+        RebarController IShutdownResourceHost.RebarController { get => rebarController; set => rebarController = value; }
+        NativeWindowController IShutdownResourceHost.TravelButtonController { get => travelBtnController; set => travelBtnController = value; }
+        IntPtr IShutdownResourceHost.BandHandle => Handle;
+        IntPtr IShutdownResourceHost.ExplorerHandle => ExplorerHandle;
+        Cursor IShutdownResourceHost.TabDragCursor { get => curTabDrag; set => curTabDrag = value; }
+        Cursor IShutdownResourceHost.TabCloningCursor { get => curTabCloning; set => curTabCloning = value; }
+        DropTargetWrapper IShutdownResourceHost.DropTargetWrapper { get => dropTargetWrapper; set => dropTargetWrapper = value; }
+        TabSwitchForm IShutdownResourceHost.TabSwitcher { get => tabSwitcher; set => tabSwitcher = value; }
+        Cursor IShutdownResourceHost.CurrentCursor { set => Cursor = value; }
+        bool IShutdownPersistenceHost.IsShown => IsShown;
+        void IShutdownPersistenceHost.UninstallHooks() => _hookInputController.Uninstall();
+        void IShutdownPersistenceHost.AddToHistory(QTabItem item) => AddToHistory(item);
+        ITravelLogStg IShutdownPersistenceHost.TravelLog { get => TravelLog; set => TravelLog = value; }
+        ShellContextMenu IShutdownPersistenceHost.ShellContextMenu { get => shellContextMenu; set => shellContextMenu = value; }
+        ShellBrowserEx IShutdownPersistenceHost.ShellBrowser { get => ShellBrowser; set => ShellBrowser = value; }
+        Dictionary<int, ITravelLogEntry> IShutdownPersistenceHost.LogEntryDic => LogEntryDic;
+        void IShutdownPersistenceHost.SetFinalRelease() => fFinalRelease = true;
+        void IShutdownPersistenceHost.CloseDWBase(uint dwReserved) => CloseDWBase(dwReserved);
         internal TreeViewWrapper ShutdownTreeViewWrapper {
             get { return treeViewWrapper; }
             set { treeViewWrapper = value; }
